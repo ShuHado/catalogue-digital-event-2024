d81d30f2b4f6dac866e008f21bf4c87d68a31f47
Cyril Arrachequesne <cyrilou564@gmail.com>
2024-02-06 22:54:47 +0100

    update responsive

--- src/mobile-embed.js
+++ src/mobile-embed.js
@@ -5,49 +5,46 @@ const modalContent = document.getElementsByClassName("modal-content");
 const main = document.getElementsByClassName("main");
 
 document.addEventListener("DOMContentLoaded", function () {
-  showEmbedParagraph.addEventListener("click", function () {
-    // MAIN
-    main[0].style.display = "none";
+	showEmbedParagraph.addEventListener("click", function () {
+		// MAIN
+		main[0].style.display = "none";
 
-    // MODAL
-    modalContent[0].style.width = "95%";
-    modalContent[0].style.height = "95%";
-    modalContent[0].style.position = "fixed";
-    modalContent[0].style.top = "50%";
-    modalContent[0].style.left = "50%";
-    modalContent[0].style.transform = "translate(-50%, -50%)";
-    modalContent[0].style.margin = "0";
-    modalContent[0].style.zIndex = "9999";
+		// MODAL
+		modalContent[0].style.width = "95%";
+		modalContent[0].style.height = "95%";
+		modalContent[0].style.position = "fixed";
+		modalContent[0].style.top = "50%";
+		modalContent[0].style.left = "50%";
+		modalContent[0].style.transform = "translate(-50%, -50%)";
+		modalContent[0].style.margin = "0";
+		modalContent[0].style.zIndex = "9999";
 
-    // EMBED
-    embedElement[0].style.display = "block";
+		// EMBED
+		embedElement[0].style.display = "block";
 
-    // CLOSE BUTTON
-    closeButton[0].style.display = "block";
+		// CLOSE BUTTON
+		closeButton[0].style.display = "block";
+	});
 
-    // FOOTER
-    footer[0].style.zIndex = "-1";
-  });
+	//   onclick close button
+	closeButton[0].addEventListener("click", function () {
+		// MAIN
+		main[0].style.display = "block";
 
-  //   onclick close button
-  closeButton[0].addEventListener("click", function () {
-    // MAIN
-    main[0].style.display = "block";
+		// MODAL
+		modalContent[0].style.position = "relative";
+		modalContent[0].style.margin = "30% 0 0 10%";
+		modalContent[0].style.height = "40dvh";
+		modalContent[0].style.width = "55%";
+		modalContent[0].style.top = "unset";
+		modalContent[0].style.left = "unset";
+		modalContent[0].style.transform = "unset";
+		modalContent[0].style.zIndex = "999";
 
-    // MODAL
-    modalContent[0].style.position = "relative";
-    modalContent[0].style.margin = "30% 0 0 10%";
-    modalContent[0].style.height = "40dvh";
-    modalContent[0].style.width = "55%";
-    modalContent[0].style.top = "unset";
-    modalContent[0].style.left = "unset";
-    modalContent[0].style.transform = "unset";
-    modalContent[0].style.zIndex = "999";
+		// EMBED
+		embedElement[0].style.display = "none";
 
-    // EMBED
-    embedElement[0].style.display = "none";
-
-    // CLOSE BUTTON
-    closeButton[0].style.display = "none";
-  });
+		// CLOSE BUTTON
+		closeButton[0].style.display = "none";
+	});
 });
